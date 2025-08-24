321d2f1e35b1051d2f20f0badae6de914449b6b5
wavrik <wavrik@example.com>
2025-08-24 13:46:23 -0300

    Atualizações

--- docker/dockerfiles/vite.Dockerfile
+++ docker/dockerfiles/vite.Dockerfile
@@ -4,6 +4,7 @@ ENV PNPM_HOME="/root/.local/share/pnpm"
 ENV PATH="$PNPM_HOME:$PATH"
 
 RUN chmod +x docker/entrypoints/vite.sh
+RUN gem install bundler -v '2.5.11'
 
 EXPOSE 3036
 CMD ["bin/vite", "dev"]
--- docker/entrypoints/vite.sh
+++ docker/entrypoints/vite.sh
@@ -4,9 +4,14 @@ set -x
 rm -rf /app/tmp/pids/server.pid
 rm -rf /app/tmp/cache/*
 
+git config --global --add safe.directory /app
+
+export PATH=/usr/local/lib/ruby/gems/3.4.0/bin:$PATH
+gem install bundler -v 2.5.11
+
 pnpm store prune
 pnpm install --force
 
 echo "Ready to run Vite development server."
 
-exec "$@"
+exec bundle _2.5.11_ exec "$@"
